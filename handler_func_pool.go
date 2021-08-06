79521f5563aa730b21cb4ba88676e3f462c38f2b
sdaf47 <sdaf47@yandex.ru>
2021-08-06 12:43:37 +0300

    refactor: Allocate handlers slice

--- handler_func_pool.go
+++ handler_func_pool.go
@@ -56,6 +56,7 @@ func (p *HandlerPool) handlersByMsgType(msgType string) (result []HandlerFunc) {
 		return
 	}
 
+	result = make([]HandlerFunc, len(handlers))
 	for _, handler := range handlers {
 		result = append(result, handler)
 	}
